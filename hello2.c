abda0a749b587a18d9d328e5ad28c789ebd2f177
Art Navsegda <artnavsegda@gmail.com>
2020-01-20 19:26:09 +0300

    Update hello2.c

--- hello2.c
+++ hello2.c
@@ -3,6 +3,10 @@
 int
 main (void)
 {
+  FILE * jsmain = fopen("./hello.js","r");
+  char buf[1000];
+  fread(buf,1000,1,jsmain);
+
   const jerry_char_t script[] = "var str = 'Hello, World!';";
 
   bool ret_value = jerry_run_simple (script, sizeof (script) - 1, JERRY_INIT_EMPTY);
