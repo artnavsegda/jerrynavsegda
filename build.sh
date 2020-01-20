fff73b08ea160be535c4884e83b6724b38f186c6
Art Navsegda <artnavsegda@gmail.com>
2020-01-20 18:59:29 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 gcc hello.c -o hello $(pkg-config --cflags --libs libjerry-core libjerry-ext libjerry-port-default libjerry-libm)
 
-gcc api-example-1.c -o api-example-1 $(pkg-config --cflags --libs libjerry-core libjerry-port-default libjerry-libm)
+gcc hello2.c -o hello2 $(pkg-config --cflags --libs libjerry-core libjerry-port-default libjerry-libm)
 
