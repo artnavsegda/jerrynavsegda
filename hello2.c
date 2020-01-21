6864fa8299ae672c1e33a6da4c0581e7f1d8dbf2
Art Navsegda <artnavsegda@gmail.com>
2020-01-21 09:30:00 +0300

    fix

--- hello2.c
+++ hello2.c
@@ -1,15 +1,13 @@
+#include <string.h>
 #include "jerryscript.h"
 
-int
-main (void)
+int main (void)
 {
   FILE * jsmain = fopen("./hello.js","r");
   char buf[1000];
   fread(buf,1000,1,jsmain);
 
-  const jerry_char_t script[] = "var str = 'Hello, World!';";
-
-  bool ret_value = jerry_run_simple (script, sizeof (script) - 1, JERRY_INIT_EMPTY);
+  bool ret_value = jerry_run_simple (buf, strlen(buf), JERRY_INIT_EMPTY);
 
   return (ret_value ? 0 : 1);
 }
