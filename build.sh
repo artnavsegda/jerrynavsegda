ac670b835672058f08ef5acd958df78ae0e45290
Art Navsegda <artnavsegda@gmail.com>
2020-01-21 09:53:55 +0300

    a

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 gcc hello.c -o hello $(pkg-config --cflags --libs libjerry-core libjerry-ext libjerry-port-default libjerry-libm)
 
-gcc hello2.c -o hello2 $(pkg-config --cflags --libs libjerry-core libjerry-port-default libjerry-libm)
-
+gcc hello2.c -o hello2 $(pkg-config --cflags --libs libjerry-core libjerry-ext libjerry-port-default libjerry-libm)
--- hello2.c
+++ hello2.c
@@ -1,38 +1,6 @@
 #include <string.h>
-#include <stdio.h>
 #include "jerryscript.h"
-
-static jerry_value_t
-print_handler (const jerry_value_t function_object,
-               const jerry_value_t function_this,
-               const jerry_value_t arguments[],
-               const jerry_length_t arguments_count)
-{
-  /* There should be at least one argument */
-  if (arguments_count > 0)
-  {
-    /* Convert the first argument to a string (JS "toString" operation) */
-    jerry_value_t string_value = jerry_value_to_string (arguments[0]);
-
-    /* A naive allocation of buffer for the string */
-    jerry_char_t buffer[256];
-
-    /* Copy the whole string to the buffer, without a null termination character,
-     * Please note that if the string does not fit into the buffer nothing will be copied.
-     * More details on the API reference page
-     */
-    jerry_size_t copied_bytes = jerry_string_to_utf8_char_buffer (string_value, buffer, sizeof (buffer) - 1);
-    buffer[copied_bytes] = '\0';
-
-    /* Release the "toString" result */
-    jerry_release_value (string_value);
-
-    printf ("%s\n", (const char *)buffer);
-  }
-
-  /* Return an "undefined" value to the JavaScript engine */
-  return jerry_create_undefined ();
-}
+#include "jerryscript-ext/handler.h"
 
 int
 main (void)
@@ -44,28 +12,9 @@ main (void)
   /* Initialize engine */
   jerry_init (JERRY_INIT_EMPTY);
 
-  /* Add the "print" method for the JavaScript global object */
-  {
-    /* Get the "global" object */
-    jerry_value_t global_object = jerry_get_global_object ();
-    /* Create a "print" JS string */
-    jerry_value_t property_name_print = jerry_create_string ((const jerry_char_t *) "print");
-    /* Create a function from a native C method (this function will be called from JS) */
-    jerry_value_t property_value_func = jerry_create_external_function (print_handler);
-    /* Add the "print" property with the function value to the "global" object */
-    jerry_value_t set_result = jerry_set_property (global_object, property_name_print, property_value_func);
-
-    /* Check if there was no error when adding the property (in this case it should never happen) */
-    if (jerry_value_is_error (set_result)) {
-      printf ("Failed to add the 'print' property\n");
-    }
-
-    /* Release all jerry_value_t-s */
-    jerry_release_value (set_result);
-    jerry_release_value (property_value_func);
-    jerry_release_value (property_name_print);
-    jerry_release_value (global_object);
-  }
+  /* Register 'print' function from the extensions to the global object */
+  jerryx_handler_register_global ((const jerry_char_t *) "print",
+                                  jerryx_handler_print);
 
   /* Setup Global scope code */
   jerry_value_t parsed_code = jerry_parse (NULL, 0, buf, strlen(buf), JERRY_PARSE_NO_OPTS);
