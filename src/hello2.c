06d5ddff60afca63495bf9151072ff182e6ebb60
Art Navsegda <artnavsegda@gmail.com>
2020-03-23 10:36:54 +0300

    art naavsegda

--- src/hello2.c
+++ src/hello2.c
@@ -2,14 +2,48 @@
 #include "jerryscript.h"
 #include "jerryscript-ext/handler.h"
 
+struct my_struct
+{
+  const char *msg;
+} my_struct;
+
+/**
+ * Get a string from a native object
+ */
+static jerry_value_t
+get_msg_handler (const jerry_value_t func_value, /**< function object */
+                 const jerry_value_t this_value, /**< this arg */
+                 const jerry_value_t *args_p, /**< function arguments */
+                 const jerry_length_t args_cnt) /**< number of function arguments */
+{
+  return jerry_create_string ((const jerry_char_t *) my_struct.msg);
+} /* get_msg_handler */
+
+static jerry_value_t
+joke_handler (const jerry_value_t function_object,
+               const jerry_value_t function_this,
+               const jerry_value_t arguments[],
+               const jerry_length_t argument_count)
+{
+  /* No arguments are used in this example */
+  /* Print out a static string */
+  printf ("Joke handler was called\n");
+
+  /* Return an "undefined" value to the JavaScript engine */
+  return jerry_create_undefined ();
+}
+
 int main (void)
 {
   FILE * jsmain = fopen("./hello.js","r");
   char buf[1000];
   fread(buf,1000,1,jsmain);
+  fclose(jsmain);
 
   /* Initializing JavaScript environment */
   jerry_init (JERRY_INIT_EMPTY);
+
+
 
   /* Register 'print' function from the extensions */
   jerryx_handler_register_global ((const jerry_char_t *) "print",
@@ -17,6 +51,9 @@ int main (void)
 
   /* Getting pointer to the Global object */
   jerry_value_t global_object = jerry_get_global_object ();
+
+  /* Create a JS function object and wrap into a jerry value */
+  jerry_value_t func_obj = jerry_create_external_function (get_msg_handler);
 
   /* Constructing strings */
   jerry_value_t prop_name = jerry_create_string ((const jerry_char_t *) "my_var");
@@ -29,6 +66,22 @@ int main (void)
     printf ("Failed to add the 'my_var' property\n");
   }
   jerry_release_value (set_result);
+
+  jerry_value_t property_name_joke = jerry_create_string ((const jerry_char_t *) "joke");
+  jerry_value_t property_value_func = jerry_create_external_function (joke_handler);
+
+  /* Add the "joke" property with the function value to the "global" object */
+  set_result = jerry_set_property (global_object, property_name_joke, property_value_func);
+
+  /* Check if there was no error when adding the property (in this case it should never happen) */
+  if (jerry_value_is_error (set_result)) {
+    printf ("Failed to add the 'joke' property\n");
+  }
+
+  jerry_release_value (set_result);
+
+  jerry_release_value (property_value_func);
+  jerry_release_value (property_name_joke);
 
   /* Releasing string values, as it is no longer necessary outside of engine */
   jerry_release_value (prop_name);
