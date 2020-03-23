ebecf2f5d62f5ad9534202a6f2abb61f1a7a1e3d
Art Navsegda <artnavsegda@gmail.com>
2020-03-23 10:43:02 +0300

    aaaa

---
+++ src/hello.js
@@ -0,0 +1,12 @@
+var myobj = {one:1, two:2};
+
+print(myobj.one + myobj.two);
+
+print (my_var);
+
+joke();
+
+var str = MyObject.myFunc ();
+print (str);
+
+print ('Hello from JS with ext!');
--- src/hello2.c
+++ src/hello2.c
@@ -46,17 +46,35 @@ int main (void)
 
 
   /* Register 'print' function from the extensions */
-  jerryx_handler_register_global ((const jerry_char_t *) "print",
-                                  jerryx_handler_print);
+  jerryx_handler_register_global ((const jerry_char_t *) "print", jerryx_handler_print);
 
-  /* Getting pointer to the Global object */
-  jerry_value_t global_object = jerry_get_global_object ();
+  /* Do something with the native object */
+  my_struct.msg = "Hello native object!";
+
+  /* Create an empty JS object */
+  jerry_value_t object = jerry_create_object ();
 
   /* Create a JS function object and wrap into a jerry value */
   jerry_value_t func_obj = jerry_create_external_function (get_msg_handler);
 
+  /* Set the native function as a property of the empty JS object */
+  jerry_value_t prop_name = jerry_create_string ((const jerry_char_t *) "myFunc");
+  jerry_release_value (jerry_set_property (object, prop_name, func_obj));
+  jerry_release_value (prop_name);
+  jerry_release_value (func_obj);
+
+  /* Wrap the JS object (not empty anymore) into a jerry api value */
+  jerry_value_t global_object = jerry_get_global_object ();
+
+  /* Add the JS object to the global context */
+  prop_name = jerry_create_string ((const jerry_char_t *) "MyObject");
+  jerry_release_value (jerry_set_property (global_object, prop_name, object));
+  jerry_release_value (prop_name);
+  jerry_release_value (object);
+  jerry_release_value (global_object);
+
   /* Constructing strings */
-  jerry_value_t prop_name = jerry_create_string ((const jerry_char_t *) "my_var");
+  prop_name = jerry_create_string ((const jerry_char_t *) "my_var");
   jerry_value_t prop_value = jerry_create_string ((const jerry_char_t *) "Hello from C!");
 
   /* Setting the string value as a property of the Global object */
