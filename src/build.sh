6dc9d94d8f51dc30aa85f8baa5f75666ebeb4eb8
Art Navsegda <artnavsegda@gmail.com>
2021-12-08 08:34:42 +0300

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gcc hello.c -o hello $(pkg-config --cflags --libs libjerry-core libjerry-ext libjerry-port-default libjerry-libm)
+gcc hello.c -o hello $(pkg-config --cflags --libs libjerry-core libjerry-ext libjerry-port-default)
 
-gcc cli.c -o cli $(pkg-config --cflags --libs libjerry-core libjerry-ext libjerry-port-default libjerry-libm) -lreadline
+#gcc cli.c -o cli $(pkg-config --cflags --libs libjerry-core libjerry-ext libjerry-port-default libjerry-libm) -lreadline
